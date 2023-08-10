7ec3c76f25ac71040171792ceb098cb4e5e0d9da
Irwan Muhamad Faizal <irwan279217@gmail.com>
2023-08-11 05:51:12 +0700

    [IMPROVE] instal axios, base dll

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
+import { NativeBaseProvider, Box } from "native-base";
 import {
   SafeAreaView,
   ScrollView,
@@ -61,29 +62,18 @@ const App: () => Node = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="langkah pertama">
+          <Section title="langkah awal">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+
         </View>
       </ScrollView>
     </SafeAreaView>
